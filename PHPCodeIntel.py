336563d1c1748dbfe067ce91cb1a5a794530ba52
Devon Weller <dweller@devonweller.com>
2013-05-04 10:28:17 -0500

    refactored plugin code to support project-level settings

--- PHPCodeIntel.py
+++ PHPCodeIntel.py
@@ -8,113 +8,144 @@ import socket
 import json
 import time
 
-settings = sublime.load_settings("PHPCodeIntel.sublime-settings")
-bin_path = sublime.packages_path() + '/PHPCodeIntel/PHP/bin'
 
 st_version = 2
 if sublime.version() == '' or int(sublime.version()) > 3000:
     st_version = 3
 
-# output debugging info to the console
-def debug_message(msg):
-  if settings.get('debug_enabled', False) == True:
-    print("[PHPCodeIntel] " + str(msg))
 
-# find the top level folder in sublime
-def find_top_folder(view, filename):
-    folders = view.window().folders()
-    path = os.path.dirname(filename)
+##############################################################################################################################
+# base plugun
+##############################################################################################################################
 
-    # We don't have any folders open, return the folder this file is in
-    if len(folders) == 0:
+class PhpCodeIntelBase:
+
+    def loadSettings(self, view):
+        self.settings = sublime.load_settings("PHPCodeIntel.sublime-settings")
+        self.project_settings = view.settings().get('PHPCodeIntel', {})
+
+        self.bin_path = sublime.packages_path() + '/PHPCodeIntel/PHP/bin'
+        # self.debugMsg("self.settings="+json.dumps(self.settings))
+
+    def getSetting(self, name, default):
+        value = self.project_settings.get(name)
+        if value == None:
+            value = self.settings.get(name)
+        if value == None:
+            value = default
+        return value
+
+    # output debugging info to the console
+    def debugMsg(self, msg):
+      if self.getSetting('debug_enabled', False) == True:
+        print("[PHPCodeIntel] " + str(msg))
+
+    def warnMsg(self, msg):
+        print("[PHPCodeIntel] WARN: " + str(msg))
+
+    # find the top level folder in sublime
+    def getProjectRoot(self, view, filename):
+        folders = view.window().folders()
+        path = os.path.dirname(filename)
+
+        # We don't have any folders open, return the folder this file is in
+        if len(folders) == 0:
+            return path
+
+        oldpath = ''
+        while not self.reachedTopLevelFolder(folders, oldpath, path):
+            oldpath = path
+            path = os.path.dirname(path)
         return path
 
-    oldpath = ''
-    while not reached_top_level_folders(folders, oldpath, path):
-        oldpath = path
-        path = os.path.dirname(path)
-    return path
-
-# helper function for find_top_level_folder
-def reached_top_level_folders(folders, oldpath, path):
-    if oldpath == path:
-        return True
-    for folder in folders:
-        if folder[:len(path)] == path:
+    # helper function for getProjectRoot
+    def reachedTopLevelFolder(self, folders, oldpath, path):
+        if oldpath == path:
             return True
-        if path == os.path.dirname(folder):
-            return True
-    return False
+        for folder in folders:
+            if folder[:len(path)] == path:
+                return True
+            if path == os.path.dirname(folder):
+                return True
+        return False
 
 
-# sends a remote command to the php daemon
-#  and returns the result
-def run_remote_cmd(command, args):
-    payload = {}
-    payload['cmd'] = command
-    payload['args'] = args
-    response = json.loads(send_message_to_php_daemon(json.dumps(payload)))
-    debug_message("run_remote_cmd response: "+json.dumps(response['msg']))
-    return response['msg']
+    # sends a remote command to the php daemon
+    #  and returns the result
+    def runRemoteCommandInPHPDaemon(self, command, args):
+        payload = {}
+        payload['cmd'] = command
+        payload['args'] = args
+        json_string = self.sendMessageToPHPDaemon(json.dumps(payload))
+        if json_string == None:
+            self.debugMsg("self.runRemoteCommandInPHPDaemon response: None")
+            return
+        response = json.loads(json_string)
+        self.debugMsg("self.runRemoteCommandInPHPDaemon response: "+json.dumps(response['msg']))
+        return response['msg']
 
 
-# connects to the socket, sends the message and returns the result
-def send_message_to_php_daemon(message):
-    try:
-        sock = connect_to_socket()
-    except Exception, e:
-        if e.errno == 61:
-            # connection refused - try restarting daemon
-            debug_message("starting daemon")
-            start_php_daemon()
+    # connects to the socket, sends the message and returns the result
+    def sendMessageToPHPDaemon(self, message):
+        sock = None
+        try:
+            sock = self.connectToSocket()
+        except Exception, e:
+            if 'errno' in e:
+                if e.errno == 61:
+                    # connection refused - try restarting daemon
+                    self.debugMsg("starting daemon")
+                    self.startPHPDaemon()
 
-            # wait 250ms for daemon to start
-            time.sleep(0.25)
+                    # wait 250ms for daemon to start
+                    time.sleep(0.25)
 
-            # connect again
-            sock = connect_to_socket()
-
-
-    netstring = str(len(message))+":"+message+","
-    sent = sock.send(netstring)
-
-    data = ''
-    while True:
-        chunk = sock.recv(1024)
-        if not chunk:
-            break
-        data = data + chunk
-
-    sock.close()
-    return data
-
-# initiates a socket connection
-def connect_to_socket():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(5)
-    sock.connect(('127.0.0.1', settings.get('port', 20001)))
-    return sock
+                    # connect again
+                    sock = self.connectToSocket()
 
 
-# starts the PHP daemon that processes commands
-def start_php_daemon():
-    debug_message("startPHPDaemon")
+        if not sock:
+            self.warnMsg("unable to connect to socket on port "+str(self.getSetting('port', 20001)))
+            return
 
-    args = []
-    args.append("php")
-    args.append("daemon.php")
+        netstring = str(len(message))+":"+message+","
+        sent = sock.send(netstring)
 
-    # Hide the console window on Windows
-    startupinfo = None
-    if os.name == "nt":
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        data = ''
+        while True:
+            chunk = sock.recv(1024)
+            if not chunk:
+                break
+            data = data + chunk
 
-    proc_env = os.environ.copy()
-    debug_message("starting proc " + ' '.join(args))
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE, startupinfo=startupinfo, env=proc_env, cwd=bin_path)
+        sock.close()
+        return data
+
+    # initiates a socket connection
+    def connectToSocket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect(('127.0.0.1', self.getSetting('port', 20001)))
+        return sock
 
 
+    # starts the PHP daemon that processes commands
+    def startPHPDaemon(self):
+        self.debugMsg("startPHPDaemon")
+
+        args = []
+        args.append("php")
+        args.append("daemon.php")
+
+        # Hide the console window on Windows
+        startupinfo = None
+        if os.name == "nt":
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        proc_env = os.environ.copy()
+        self.debugMsg("starting proc " + ' '.join(args))
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE, startupinfo=startupinfo, env=proc_env, cwd=self.bin_path)
 
 
 
@@ -124,13 +155,15 @@ def start_php_daemon():
 ##############################################################################################################################
 
 # scans a single file
-class PhpCodeIntelScanFileCommand(sublime_plugin.TextCommand):
+class PhpCodeIntelScanFileCommand(PhpCodeIntelBase, sublime_plugin.TextCommand):
+
     def run(self, edit):
+        self.loadSettings(self.view)
         src_file = self.view.file_name()
         if src_file == None:
             return
-        dest_file = find_top_folder(self.view, src_file) + '/.php_intel.sqlite3'
-        run_remote_cmd('scanFile', [src_file, dest_file])
+        dest_file = self.getProjectRoot(self.view, src_file) + '/.php_intel.sqlite3'
+        self.runRemoteCommandInPHPDaemon('scanFile', [src_file, dest_file])
 
 
 
@@ -138,12 +171,14 @@ class PhpCodeIntelScanFileCommand(sublime_plugin.TextCommand):
 # Autocomplete
 ##############################################################################################################################
 
-class PhpCodeIntelAutoComplete(sublime_plugin.EventListener):
+class PhpCodeIntelAutoComplete(PhpCodeIntelBase, sublime_plugin.EventListener):
+
     def on_query_completions(self, view, prefix, locations):
-        if settings.get('autocomplete_enabled', False) == True:
+        self.loadSettings(view)
+        if self.getSetting('autocomplete_enabled', False) == True:
             src_file = view.file_name()
-            php_intel_file = find_top_folder(view, src_file) + '/.php_intel.sqlite3'
-            completions_array = run_remote_cmd('autoComplete', ['', php_intel_file])
+            php_intel_file = self.getProjectRoot(view, src_file) + '/.php_intel.sqlite3'
+            completions_array = self.runRemoteCommandInPHPDaemon('autoComplete', ['', php_intel_file])
 
             # convert completions array into tuples for python
             completions = []
@@ -151,5 +186,4 @@ class PhpCodeIntelAutoComplete(sublime_plugin.EventListener):
                 completions.append((item[0], item[1]))
             return completions
 
-debug_message("Plugin Loaded")
 
