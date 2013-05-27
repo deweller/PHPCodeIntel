85be4d1933d57310efb6e11d7969c882cfb0b2b9
Devon Weller <dweller@devonweller.com>
2013-05-27 07:16:29 -0500

    added more descriptive labels of completion context for sublime

--- PHP/lib/local/PHPIntel/Completions/Formatter.php
+++ PHP/lib/local/PHPIntel/Completions/Formatter.php
@@ -14,11 +14,46 @@ class Formatter
     public function formatEntitiesAsCompletions($entities) {
         $out = array();
         foreach($entities as $entity) {
-            $out[] = array($entity['name']."\t"."function", $this->escapeForSublime($entity['completion']));
+            $out[] = array($entity['name']."\t".$this->formatTypeForSublime($entity), $this->escapeForSublime($entity['completion']));
         }
         return $out;
     }
 
+    protected function formatTypeForSublime($entity) {
+        // *   type: method, variable, constant
+        // *   visibility: public, protected, private
+        // *   scope: instance or static
+
+        switch ($entity['type']) {
+            case 'method':
+                return $this->abbreviatedVisibility($entity['visibility']).' func';
+            break;
+            case 'variable':
+                return $this->abbreviatedVisibility($entity['visibility']).' var';
+            break;
+            case 'constant':
+                return $this->abbreviatedVisibility($entity['visibility']).' const';
+            break;
+        }
+
+        return 'unknown';
+    }
+
+    protected function abbreviatedVisibility($full_visibility) {
+        switch ($full_visibility) {
+            case 'public':
+                return 'pub';
+            break;
+            case 'protected':
+                return 'pro';
+            break;
+            case 'private':
+                return 'priv';
+            break;
+        }
+
+        return '';
+    }
 
     protected function escapeForSublime($text) {
       return str_replace('$','\\$', $text);
