3d568f0bb87682ac88dfc79fe038f0dafb72b9b6
Devon Weller <dweller@devonweller.com>
2013-05-29 07:09:13 -0500

    added abbreviation for static scope

--- PHP/lib/local/PHPIntel/Completions/Formatter.php
+++ PHP/lib/local/PHPIntel/Completions/Formatter.php
@@ -10,7 +10,6 @@ use \Exception;
 */
 class Formatter
 {
-
     public function formatEntitiesAsCompletions($entities) {
         $out = array();
         foreach($entities as $entity) {
@@ -26,29 +25,38 @@ class Formatter
 
         switch ($entity['type']) {
             case 'method':
-                return $this->abbreviatedVisibility($entity['visibility']).' func';
+                return $this->abbreviatedVisibilityAndScope($entity['visibility'], $entity['scope']).' func';
             break;
             case 'variable':
-                return $this->abbreviatedVisibility($entity['visibility']).' var';
+                return $this->abbreviatedVisibilityAndScope($entity['visibility'], $entity['scope']).' var';
             break;
             case 'constant':
-                return $this->abbreviatedVisibility($entity['visibility']).' const';
+                return $this->abbreviatedVisibilityAndScope($entity['visibility'], $entity['scope']).' const';
             break;
         }
 
         return 'unknown';
     }
 
-    protected function abbreviatedVisibility($full_visibility) {
+    protected function abbreviatedVisibilityAndScope($full_visibility, $full_scope) {
+        switch ($full_scope) {
+            case 'instance':
+                $scope_prefix = '';
+                break;
+            case 'static':
+                $scope_prefix = ' st';
+                break;
+        }
+
         switch ($full_visibility) {
             case 'public':
-                return 'pub';
+                return 'pub'.$scope_prefix;
             break;
             case 'protected':
-                return 'pro';
+                return 'pro'.$scope_prefix;
             break;
             case 'private':
-                return 'priv';
+                return 'priv'.$scope_prefix;
             break;
         }
 
